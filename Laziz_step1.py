a933c1bec84a3d5aa7413712daba2c7a66915716
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2022-09-22 11:20:13 +0100

    Update Laziz_step1.py

--- Laziz_step1.py
+++ Laziz_step1.py
@@ -3,5 +3,5 @@ from muselsl import stream, list_muses
 muses = list_muses()
 stream(muses[0]['address'])
 
-# Note: Streaming is synchronous, so code here will not execute until after the stream has been closed
+# Note: Streaming is synchronous, so the code here will not execute until after the stream has been closed
 print('Stream has ended')
